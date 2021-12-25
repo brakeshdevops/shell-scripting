66ad54fab22df7a7012944f244eaa55d046daf70
brakesh <bandarupallirakesh@gmail.com>
2021-12-25 17:03:16 +0530

    color coding added

--- shell3.sh
+++ shell3.sh
@@ -7,3 +7,7 @@ a=(10 20 30 40 50)
 sum=${a[0]}+${a[1]}
 echo "${a[0]}"
 echo ${sum}
+read -p "Enter your name" name
+read -p "ENter your age" age
+echo "your name is ${name} and your age is ${age}"
+
