9dafd2b4e6d3e0c821fe090d3c2189a8f90e8e24
brakesh <bandarupallirakesh@gmail.com>
2021-12-25 11:46:08 +0530

    color coding added

--- shell2.sh
+++ shell2.sh
@@ -1,4 +1,4 @@
 echo Welcome to shell scripting
 echo This is the second line
 echo -e "\e[31mWelcome to shell scripting\e[0m"
-echo -e "\e\[32m This is second color coding\e[0m"
+echo -e "\e[32mThis is second color coding\e[0m"
