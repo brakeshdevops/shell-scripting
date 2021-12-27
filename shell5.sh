3ec0a6e0443ef711048d909c7147a19100359e24
brakesh <bandarupallirakesh@gmail.com>
2021-12-27 23:08:16 +0530

    Conditional shell scripting

--- shell5.sh
+++ shell5.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 read -p "Enter the first no" a
 read -p "Enter the second no" b
+read -p "Enter the third no" c
+if [ -z "$a" ]; then
+  echo "A is empty"
+  exit
+fi
 if [ ${a} -gt ${b} ]; then
   echo "A is the greatest no"
+elif [ ${b} -gt ${c} ]; then
+  echo "B is greatest no"
+else
+  echo "C is greatest no"
 fi
