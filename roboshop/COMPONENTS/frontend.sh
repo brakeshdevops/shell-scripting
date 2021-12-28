5a83fb5472ef61188ad6ca62bf4f31c82c5376b5
brakesh <bandarupallirakesh@gmail.com>
2021-12-28 12:23:49 +0530

    frontend code modified and STAT_CHECK function added here

--- roboshop/COMPONENTS/frontend.sh
+++ roboshop/COMPONENTS/frontend.sh
@@ -1,10 +1,10 @@
 STAT_CHECk()
 {
   if [ $1 -ne 0 ]; then
-    echo -e "\e[1;31m$2\e[0"-failed
+    echo -e "\e[1;31m$2-failed\e[0m"
     exit 1
   else
-    echo -e "\e[1;32m$2\e[0"-Success
+    echo -e "\e[1;32m$2-Success\e[0m"
   fi
 }
 yum install nginx -y
