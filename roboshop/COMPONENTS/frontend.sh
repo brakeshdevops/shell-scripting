521f77f00ef80a8d78c3ad1412a0f3631c729111
brakesh <bandarupallirakesh@gmail.com>
2021-12-28 21:53:44 +0530

    frontend code modified and STAT_CHECK function added here

--- roboshop/COMPONENTS/frontend.sh
+++ roboshop/COMPONENTS/frontend.sh
@@ -11,16 +11,15 @@ STAT_CHECK()
 }
 yum install nginx -y &>>${LOG_FILE}
 STAT_CHECK $? "Nginx installation"
-systemctl enable nginx
-systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${LOG_FILE}
 STAT_CHECK $? "Download frontend"
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-master static README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-systemctl enable nginx
-systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+STAT_CHECK $? "Remove old html files"
+cd /tmp && unzip /tmp/frontend.zip &>>${LOG_FILE}
+STAT_CHECK $? "Extracting Frontend content"
+cd /tmp/frontend-main/static/ && cp -r * /usr/share/nginx/html/
+STAT_CHECK $? "copying front end content"
+cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT_CHECK $? "Updating nginx config file"
+systemctl enable nginx &>>${LOG_FILE} && systemctl restart nginx &>>${LOG_FILE}
+STAT_CHECK $? "Restart nginx"
