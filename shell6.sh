734d1256c321595f44c07c5f2236dd0b4e6a96a3
brakesh <bandarupallirakesh@gmail.com>
2021-12-28 11:55:48 +0530

    Function shell script added

--- shell6.sh
+++ shell6.sh
@@ -3,13 +3,13 @@ function display1()
 {
   echo "This is first function program"
   echo "This is second statement in function program"
-  echo ${1}+${2}
+  echo $1+$2
 }
 display()
 {
   echo "This is a function without function keyword"
   echo "This is the second statement of the display program"
-  echo ${1}+${2}
+  echo $1+$2
 }
 display 45 56
 display1 67 78
