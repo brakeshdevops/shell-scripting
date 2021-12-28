92c18592f436c67d5beffea59bd0c7d699b33a2d
brakesh <bandarupallirakesh@gmail.com>
2021-12-28 11:59:13 +0530

    Function shell script added

--- shell6.sh
+++ shell6.sh
@@ -4,6 +4,8 @@ function display1()
   echo "This is first function program"
   echo "This is second statement in function program"
   echo $(($1+$2))
+  b=45
+  echo $(($a+96))
 }
 display()
 {
@@ -11,5 +13,7 @@ display()
   echo "This is the second statement of the display program"
   echo $(($1+$2))
 }
+a=96
+echo $(($b+77))
 display 45 56
 display1 67 78
