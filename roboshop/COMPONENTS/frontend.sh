55874caa999716f3f19da59bdbe43d8309e68d2a
brakesh <bandarupallirakesh@gmail.com>
2021-12-28 12:37:50 +0530

    frontend code modified and STAT_CHECK function added here

--- roboshop/COMPONENTS/frontend.sh
+++ roboshop/COMPONENTS/frontend.sh
@@ -1,3 +1,5 @@
+LOG_FILE=/tmp/roboshop.log
+rm -f ${LOG_FILE}
 STAT_CHECK()
 {
   if [ $1 -ne 0 ]; then
@@ -7,11 +9,11 @@ STAT_CHECK()
     echo -e "\e[1;32m$2-Success\e[0m"
   fi
 }
-yum install nginx -y
+yum install nginx -y &>>LOG_FILE
 STAT_CHECK $? "Nginx installation"
 systemctl enable nginx
 systemctl start nginx
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>LOG_FILE
 STAT_CHECK $? "Download frontend"
 cd /usr/share/nginx/html
 rm -rf *
