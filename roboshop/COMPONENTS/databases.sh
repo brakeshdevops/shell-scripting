475751b4ac7cd8157be4d89a6f53e74f2cb12af9
brakesh <bandarupallirakesh@gmail.com>
2021-12-28 22:34:30 +0530

    Mongodb added

--- roboshop/COMPONENTS/databases.sh
+++ roboshop/COMPONENTS/databases.sh
@@ -3,8 +3,8 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/robos
 STAT_CHECK $? "download mongodb repository"
 yum install -y mongodb-org &>>${LOG_FILE}
 STAT_CHECK $? "Install Mongodb"
-## systemctl enable mongod
-## systemctl start mongod
+systemctl enable mongod &>>${LOG_FILE} && systemctl start mongod &>>${LOG_FILE}
+STAT_CHECK $? "Started mongodb service"
 #Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
 #Config file: /etc/mongod.conf
 #
