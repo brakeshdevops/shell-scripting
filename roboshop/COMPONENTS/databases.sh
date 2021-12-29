45c33e6b66be3b9ff7bd6baba3f649040a93e92c
brakesh <bandarupallirakesh@gmail.com>
2021-12-29 19:08:59 +0530

    Loading schema code is added

--- roboshop/COMPONENTS/databases.sh
+++ roboshop/COMPONENTS/databases.sh
@@ -14,3 +14,15 @@ Download mongodb
 cd mongodb-main
 mongo < catalogue.js &>>${LOG_FILE} && mongo < users.js &>>${LOG_FILE}
 STAT_CHECK $? "Loading the schema"
+
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG_FILE}
+STAT_CHECK $? "Downloaded redis"
+
+yum install redis -y &>>${LOG_FILE}
+STAT_CHECk $? "Installed redis"
+
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis.conf &>>${LOG_FILE}
+STAT_CHECK $? "Update IP address"
+
+systemctl enable redis &>>${LOG_FILE} && systemctl restart redis &>>${LOG_FILE}
+STAT_CHECK $? "Start redis database"
