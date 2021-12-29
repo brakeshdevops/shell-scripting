f0f52fdb8e4dd10ce26ee81389860bbd9d43233c
brakesh <bandarupallirakesh@gmail.com>
2021-12-29 18:33:21 +0530

    Downloading frontend updated

--- roboshop/COMPONENTS/frontend.sh
+++ roboshop/COMPONENTS/frontend.sh
@@ -1,8 +1,8 @@
 source COMPONENTS/common.sh
 yum install nginx -y &>>${LOG_FILE}
 STAT_CHECK $? "Nginx installation"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${LOG_FILE}
-STAT_CHECK $? "Download frontend"
+
+Download frontend
 rm -rf /usr/share/nginx/html/*
 STAT_CHECK $? "Remove old html files"
 cd /tmp && unzip -o /tmp/frontend.zip &>>${LOG_FILE}
