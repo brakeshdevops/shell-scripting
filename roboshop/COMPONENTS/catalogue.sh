fd5da1a2a3e00e950daac78940535aa86d7b12bd
brakesh <bandarupallirakesh@gmail.com>
2021-12-30 15:05:40 +0530

    Mysql database added

---
+++ roboshop/COMPONENTS/catalogue.sh
@@ -0,0 +1,17 @@
+source COMPONENTS/common.sh
+yum install nodejs make gcc-c++ -y &>>${LOG_FILE}
+STAT_CHECK $? "Install nodejs"
+useradd roboshop &>>${LOG_FILE}
+STAT_CHECK $? "User Add"
+Download catalogue
+STAT_CHECK $? "Download Catalogue"
+#$ mv catalogue-main catalogue
+#$ cd /home/roboshop/catalogue
+#$ npm install
+#NOTE: We need to update the IP address of MONGODB Server in systemd.service file
+#Now, lets set up the service with systemctl.
+#
+## mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+## systemctl daemon-reload
+## systemctl start catalogue
+## systemctl enable catalogue
