eb18e9f56318094096c9ac20d3543275cda83499
brakesh <bandarupallirakesh@gmail.com>
2021-12-30 16:06:49 +0530

    Added catalogue file code

--- roboshop/COMPONENTS/catalogue.sh
+++ roboshop/COMPONENTS/catalogue.sh
@@ -12,7 +12,7 @@ STAT_CHECK $? "Copy catalogue content"
 cd /home/roboshop/catalogue && npm install --unsafe-perm &>>${LOG_FILE}
 STAT_CHECK $? "Install npm"
 chown roboshop:roboshop -R /home/roboshop
-sed -i -e 's/MONGO_DNSNAME/mongod.roboshop.internal' /home/roboshop/catalogue/systemd.service &>>${LOG_FILE} && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
+sed -i -e 's/MONGO_DNSNAME/mongod.roboshop.internal/' /home/roboshop/catalogue/systemd.service &>>${LOG_FILE} && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
 STAT_CHECK $? "update systemd file"
 systemctl daemon-reload &>>${LOG_FILE} && systemctl start catalogue &>>${LOG_FILE} && systemctl enable catalogue &>>${LOG_FILE}
 STAT_CHECK $? "Restart catalogue"
